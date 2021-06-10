543bb3620b42bb8c2095e34bb5fd6427c81466d9
kumarsom <somesh.kumar@tieto.com>
2021-06-10 18:45:32 +0200

    removed unused imports

--- twitter-security/src/main/java/org/twitter/security/handlers/ApiAuthenticationFailureHandler.java
+++ twitter-security/src/main/java/org/twitter/security/handlers/ApiAuthenticationFailureHandler.java
@@ -3,13 +3,11 @@ package org.twitter.security.handlers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
